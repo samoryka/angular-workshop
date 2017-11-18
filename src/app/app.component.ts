7426e4a116891de189afc6f640be7691fc0ed583
Slashgear <antoine395.caron@gmail.com>
2017-11-18 15:51:53 +0100

    Loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  private name:string = 'Antoine Caron'; 
+  private names:string[] = ['Iron Man', 'Captain America']; 
 }
